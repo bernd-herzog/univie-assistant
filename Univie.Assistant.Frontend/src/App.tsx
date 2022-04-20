68659ecc3f71b4c5d6356e7f3e0e6cd82b4d2575
Bernd Herzog <bernd@nachregenkommtsonne.de>
2022-04-20 22:04:58 +0200

    Added open street map

--- Univie.Assistant.Frontend/src/App.tsx
+++ Univie.Assistant.Frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
 
 export default class App extends React.Component<{}, {}> {
     constructor(props: any) {
@@ -7,9 +9,22 @@ export default class App extends React.Component<{}, {}> {
 
     render() {
         return <>
-            <div>
+            <MapContainer
+                style={{ width: '800px', height: '800px' }}
+                center={[48.208333, 16.373056]}
+                zoom={14} >
 
-            </div>
+                <TileLayer
+
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[48.208333, 16.373056]}>
+                    <Popup>
+                        Wien ist hier. <br /> Genau hier.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         </>;
     }
 }
